4436d89d5939081259be7078c2e136a8eff7bb6c
Santin05 <ammiragliasantino@gmail.com>
2025-05-01 17:10:39 -0300

    Se creo la herencia

---
+++ Domain/CuentaCorriente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Dsw2025Ej8.Domain.Exceptions;
+
+namespace Dsw2025Ej8.Domain
+{
+    public class CuentaCorriente : CuentaBancaria
+    {
+
+        public CuentaCorriente(string numero, decimal saldo, string[] titulares) : base(numero, saldo, titulares)
+        {
+
+        }
+        public decimal LimiteDeDescubierto { get; init; }
+        public decimal Comision { get; init; }
+
+        public override void Depositar(decimal monto)
+        {
+            ValidarCuenta();
+            ValidarMonto(monto);
+            decimal montoFinal = monto - (monto * Comision);
+            Saldo += montoFinal;
+        }
+
+        private void ValidarCuenta()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ValidarMonto(decimal monto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Retirar(decimal monto)
+        {
+            ValidarCuenta();
+            ValidarMonto(monto);
+
+            if (Saldo - monto >= -LimiteDeDescubierto)
+            {
+                Saldo -= monto;
+
+                if (Saldo < 0)
+                {
+                    Estado = Estado.Suspendida;
+                    throw new SaldoInsuficiente($"Cuenta {Numero}: La cuenta no cuenta con saldo suficiente para la operacion. Fue suspendida");
+                }
+            }
+
+        }
+    }
+
+
+}
---
+++ Domain/Exceptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain;
+
+public class Exceptions
+{
+    public class MontoNoValido : Exception
+    {
+        public MontoNoValido(string message) : base(message) { }
+    }
+
+    public class CuentaNoActiva : Exception
+    {
+        public CuentaNoActiva(string message) : base(message) { }
+    }
+
+    public class SaldoInsuficiente : Exception
+    {
+        public SaldoInsuficiente(string message) : base(message) { }
+    }
+    public static void Handle(Exception ex)
+    {
+        switch (ex)
+        {
+            case MontoNoValido mo:
+                Console.WriteLine(mo.Message);
+                break;
+            case CuentaNoActiva cu:
+                Console.WriteLine(cu.Message);
+                break;
+            case SaldoInsuficiente sa:
+                Console.WriteLine(sa.Message);
+                break;
+            default:
+                Console.WriteLine($"[ERROR DESCONOCIDO] {ex.Message}");
+                break;
+        }
+    }
+
+
+}
