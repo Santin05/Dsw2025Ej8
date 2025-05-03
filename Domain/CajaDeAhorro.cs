21692c2d11033b60cc8c1cf45236a3700e8d6700
orianaliendro <Oriana.Liendro@alu.frt.utn.edu.ar>
2025-05-02 21:43:45 -0300

    se agrego propiedades

---
+++ Domain/CajaDeAhorro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Dsw2025Ej8.Domain.Exceptions;
+
+namespace Dsw2025Ej8.Domain
+{
+    internal class CajaDeAhorro : CuentaBancaria
+    {
+        public CajaDeAhorro(string numero, decimal saldo, string[] titulares) : base(numero, saldo, titulares)
+        {
+
+        }
+        public override void Depositar(decimal monto)
+        {
+            ValidarCuenta();
+            ValidarMonto(monto);
+            Saldo += monto;
+        }
+        public override void Retirar(decimal monto)
+        {
+            ValidarCuenta();
+            ValidarMonto(monto);
+            Saldo -= monto;
+        }
+
+        private void ValidarMonto(decimal monto)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ValidarCuenta()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
